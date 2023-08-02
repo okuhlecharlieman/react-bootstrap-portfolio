0c3c20239b021ac0c947221ebaa6395a89bae791
Okuhle <okuhlecharlieman72@gmail.com>
2023-08-02 15:47:49 +0200

    layout on the WorkParticipation Component

--- src/Components/WorkParticipation.js
+++ src/Components/WorkParticipation.js
@@ -2,68 +2,74 @@ import React from "react";
 
 const WorkParticipation = () =>{
     return(
-       <div >
+<div >
         <div className="container ">
+
           <div className="Text d-flex row justify-content-end ">
             <div className="align-self-end col-lg-12 text-left mt-5" >
-                <h1 className="display-6">Work experiences</h1>
+                <h1 className="display-6">Work Experience</h1>
                 <hr />
-                               <div>
-                                 <h3>Printer Technician</h3>
-                                   <h4>Gqwa-gqwa</h4>
-                            <p>I spent four formative years at my primary school,
-                                from grade 4 through 7, where I gained a strong foundation in education and
-                                formed many cherished memories that have stayed with me to this day
-                                </p>
-                               </div>
+               
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 col-12 mt-5 pt-2">
+                        <div className="feature-widget text-left rounded">
+                            <div className="services-icon text-primary mb-3">
+                            <img width="100" height="100" 
+                            src="https://img.icons8.com/arcade/100/printer-door-open.png" 
+                            alt="printer-door-open"/>            
+                              </div>
+                            <div className="services-texts">
+                                <h3 className="title mb-3">Printer Technician</h3>
+                                <p className="text-light-muted mb-4">After completing my IT diploma, I sought out new opportunities and decided to make the move to Cape Town. It was there that I landed my first job at Raydian, where I worked as a printer technician. In this role, I was responsible for the installation and maintenance of printers, ensuring that they ran smoothly and efficiently for our clients. It was a challenging yet rewarding experience that allowed me to hone my technical skills and develop my customer service abilities.</p>
+                               
+                            </div>
+                        </div>
+                    </div>
 
-                               <div>
-       <h3>Sales Associate</h3>
-     <h4>Gqwa-gqwa</h4>
-<p>After completing my primary education,
-     I moved on to a new school where I 
-     completed my high school education from grade 8 through 12,
-      eventually earning my matric in 2017. Those five years were a time of personal growth,
-       academic challenges,
-     and memorable experiences that have helped shape who I am today
-     </p>
-       </div>
-       <div>
-       <h3>Systems Development</h3>
-<h4>Gqwa-gqwa</h4>
-<p>
-Upon graduating from matric in 2017, 
-I eagerly embarked on my tertiary education at Nelson Mandela University, 
-where I pursued a degree in Information Technology with a focus on software development. 
-I dove headfirst into my studies and relished the opportunity to expand my knowledge and skills in this dynamic field
-</p>
-       </div>
+                    <div className="col-lg-4 col-md-6 col-12   pt-2">
+                        <div className="feature-widget text-left rounded">
+                            <div className="services-icon text-primary mb-3">
+                            <img width="100" height="100" src="https://img.icons8.com/stickers/100/winter-sales.png" alt="winter-sales"/>               
+                                 </div>
+                            <div className="services-texts">
+                                <h3 className="title mb-3">Sales Associate</h3>
+                                <p className="text-light-muted mb-4">After completing my primary education,
+                                I worked as a sales associate at Total Sports, where I had the opportunity to interact with customers, understand their needs, and recommend the right solutions to help them. It was a great experience that allowed me to develop my communication and customer service skills, as well as my knowledge of various sports products and equipment.</p>
+                              
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 col-12 mt-5 pt-2">
+                        <div className="feature-widget text-left rounded">
+                            <div className="services-icon text-primary mb-3">
+                            <img width="100" height="100" src="https://img.icons8.com/fluency/100/window-settings.png" alt="window-settings"/>                            </div>
+                            <div className="services-texts">
+                                <h3 className="title mb-3">Systems Development</h3>
+                                <p className="text-light-muted mb-4">I'm currently enrolled in the systems development pathway, which is designed to bridge the skills gap in software engineering. This program offers a comprehensive curriculum that covers both front-end and back-end web development, with 12 modules aligned to SETA unit standards. I have the flexibility to select and combine modules such as HTML, CSS, JavaScript, Python, and Java to create personalized 6-month programs.
+
+Upon completion, I'll have the opportunity to earn industry certifications like Microsoft Python Certification and Oracle Java Certification. The program is fully aligned to the National Qualifications Framework (NQF) Unit Standards, which will ensure that I'm equipped with the necessary skills and knowledge to excel in this exciting field.</p>
+                            
+
+                            </div>
+                        </div>
+                    </div>
+        
+
+            
+                </div>
+                 
             </div>
           </div>
         </div>
-     
-      </div>
+       
+      </div> 
       
     );
 }
 
 export default WorkParticipation;
 
-/*   <h3>Sales Associate</h3>
-     <h4>Gqwa-gqwa</h4>
-<p>After completing my primary education,
-     I moved on to a new school where I 
-     completed my high school education from grade 8 through 12,
-      eventually earning my matric in 2017. Those five years were a time of personal growth,
-       academic challenges,
-     and memorable experiences that have helped shape who I am today
-     </p>
-     <h3>Systems Development</h3>
-<h4>Gqwa-gqwa</h4>
-<p>
-Upon graduating from matric in 2017, 
-I eagerly embarked on my tertiary education at Nelson Mandela University, 
-where I pursued a degree in Information Technology with a focus on software development. 
-I dove headfirst into my studies and relished the opportunity to expand my knowledge and skills in this dynamic field
-</p>
-    </div> */
+/* 
+
+*/
